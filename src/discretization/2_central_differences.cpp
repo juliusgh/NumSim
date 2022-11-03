020f294f4a6321f64a84df39a8eff0719efb238d
Niklas Hornischer <st161340@stud.uni-stuttgart.de>
2022-11-03 22:07:30 +0100

    fix pure virtual const members #1 and #6

--- src/discretization/2_central_differences.cpp
+++ src/discretization/2_central_differences.cpp
@@ -7,21 +7,21 @@ Discretization(nCells, meshWidth)
 }
 
 //! compute the 1st derivative ∂ u^2 / ∂x
-double CentralDifferences::computeDu2Dx(int i, int j) {
+double CentralDifferences::computeDu2Dx(int i, int j) const {
     return (u(i+1, j) * u(i+1, j) + 2 * u(i, j) * (u(i+1, j) - u(i-1, j)) - u(i-1, j) * u(i-1, j) ) / (4 * Discretization::dx());
 }
 
 //! compute the 1st derivative ∂ v^2 / ∂y
-double CentralDifferences::computeDv2Dy(int i, int j) {
+double CentralDifferences::computeDv2Dy(int i, int j) const {
     return (v(i+1, j) * v(i+1, j) + 2 * v(i, j) * (v(i+1, j) - v(i-1, j)) - v(i-1, j) * v(i-1, j) ) / (4 * Discretization::dy());
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂x
-double CentralDifferences::computeDuvDx(int i, int j) {
+double CentralDifferences::computeDuvDx(int i, int j) const {
     return ((v(i, j) + v(i+1, j)) * (u(i, j+1) + u(i, j)) - (v(i-1, j) + v(i, j)) * (u(i-1, j+1) + u(i-1, j))) / (4 * Discretization::dx());
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂y
-double CentralDifferences::computeDuvDy(int i, int j) {
+double CentralDifferences::computeDuvDy(int i, int j) const {
     return ((u(i, j+1) + u(i, j)) * (v(i+1, j) + v(i, j)) - (u(i, j) + u(i, j-1)) * (v(i+1, j) + v(i, j-1))) / (4 * Discretization::dy());
 }
--- src/discretization/2_donor_cell.cpp
+++ src/discretization/2_donor_cell.cpp
@@ -7,21 +7,21 @@ Discretization(nCells, meshWidth)
 }
 
 //! compute the 1st derivative ∂ u^2 / ∂x
-double DonorCell::computeDu2Dx(int i, int j) {
+double DonorCell::computeDu2Dx(int i, int j) const {
     return 0;
 }
 
 //! compute the 1st derivative ∂ v^2 / ∂y
-double DonorCell::computeDv2Dy(int i, int j) {
+double DonorCell::computeDv2Dy(int i, int j) const {
     return 0;
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂x
-double DonorCell::computeDuvDx(int i, int j) {
+double DonorCell::computeDuvDx(int i, int j) const {
     return 0;
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂y
-double DonorCell::computeDuvDy(int i, int j) {
+double DonorCell::computeDuvDy(int i, int j) const {
     return 0;
 }
