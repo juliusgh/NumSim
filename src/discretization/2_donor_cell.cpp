3ed6df9df3ce23ba80d9231b6066a06ef37bdf4a
Niklas Hornischer <st161340@stud.uni-stuttgart.de>
2022-11-03 21:52:54 +0100

    fix pure virtual functions and add template for code #6

---
+++ src/discretization/2_donor_cell.cpp
@@ -0,0 +1,27 @@
+#include "discretization/2_donor_cell.h"
+
+DonorCell::DonorCell(std::array<int, 2> nCells, std::array<double, 2> meshWidth, double alpha) :
+Discretization(nCells, meshWidth)
+{
+
+}
+
+//! compute the 1st derivative ∂ u^2 / ∂x
+double DonorCell::computeDu2Dx(int i, int j) {
+    return 0;
+}
+
+//! compute the 1st derivative ∂ v^2 / ∂y
+double DonorCell::computeDv2Dy(int i, int j) {
+    return 0;
+}
+
+//! compute the 1st derivative ∂ (uv) / ∂x
+double DonorCell::computeDuvDx(int i, int j) {
+    return 0;
+}
+
+//! compute the 1st derivative ∂ (uv) / ∂y
+double DonorCell::computeDuvDy(int i, int j) {
+    return 0;
+}
--- src/discretization/2_donor_cell.h
+++ src/discretization/2_donor_cell.h
@@ -10,15 +10,15 @@ public:
   DonorCell(std::array<int,2> nCells, std::array<double,2> meshWidth, double alpha);
 
   //! compute the 1st derivative ∂ u^2 / ∂x
-  virtual double computeDu2Dx(int i, int j) const;
+  virtual double computeDu2Dx(int i, int j);
 
   //! compute the 1st derivative ∂ v^2 / ∂y
-  virtual double computeDv2Dy(int i, int j) const;
+  virtual double computeDv2Dy(int i, int j);
 
   //! compute the 1st derivative ∂ (uv) / ∂x
-  virtual double computeDuvDx(int i, int j) const;
+  virtual double computeDuvDx(int i, int j);
 
   //! compute the 1st derivative ∂ (uv) / ∂y
-  virtual double computeDuvDy(int i, int j) const;
+  virtual double computeDuvDy(int i, int j);
   
 };
