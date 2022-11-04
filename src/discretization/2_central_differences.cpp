e86fec5c147bcb735545dd6b6e05373880fae101
Niklas Hornischer <st161340@stud.uni-stuttgart.de>
2022-11-04 12:11:18 +0100

    increase performance and index error #1

--- src/discretization/2_central_differences.cpp
+++ src/discretization/2_central_differences.cpp
@@ -8,20 +8,20 @@ CentralDifferences::CentralDifferences(std::array<int, 2> nCells, std::array<dou
 
 //! compute the 1st derivative ∂ u^2 / ∂x
 double CentralDifferences::computeDu2Dx(int i, int j) const {
-    return (u(i+1, j) * u(i+1, j) + 2 * u(i, j) * (u(i+1, j) - u(i-1, j)) - u(i-1, j) * u(i-1, j) ) / (4 * Discretization::dx());
+    return ((u(i+1,j) + u(i,j)) * (u(i+1,j) + u(i,j)) - (u(i,j) + u(i-1,j)) * (u(i,j) + u(i-1,j))) / (4 * Discretization::dx());
 }
 
 //! compute the 1st derivative ∂ v^2 / ∂y
 double CentralDifferences::computeDv2Dy(int i, int j) const {
-    return (v(i+1, j) * v(i+1, j) + 2 * v(i, j) * (v(i+1, j) - v(i-1, j)) - v(i-1, j) * v(i-1, j) ) / (4 * Discretization::dy());
+    return ((v(i,j+1) + v(i,j)) * (v(i,j+1) + v(i,j)) - (v(i,j) + v(i,j-1)) * (v(i,j) + v(i,j-1))) / (4 * Discretization::dy());
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂x
 double CentralDifferences::computeDuvDx(int i, int j) const {
-    return ((v(i, j) + v(i+1, j)) * (u(i, j+1) + u(i, j)) - (v(i-1, j) + v(i, j)) * (u(i-1, j+1) + u(i-1, j))) / (4 * Discretization::dx());
+    return ((v(i,j) + v(i+1,j)) * (u(i,j+1) + u(i,j)) - (v(i-1,j) + v(i,j)) * (u(i-1,j+1) + u(i-1,j))) / (4 * Discretization::dx());
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂y
 double CentralDifferences::computeDuvDy(int i, int j) const {
-    return ((u(i, j+1) + u(i, j)) * (v(i+1, j) + v(i, j)) - (u(i, j) + u(i, j-1)) * (v(i+1, j-1) + v(i, j-1))) / (4 * Discretization::dy());
+    return ((u(i,j+1) + u(i,j)) * (v(i+1,j) + v(i,j)) - (u(i,j) + u(i,j-1)) * (v(i+1,j-1) + v(i,j-1))) / (4 * Discretization::dy());
 }
