bd51b22d976c29be3d8a0b418b362900cbce0772
Julius Herb <julius.herb@gmail.com>
2022-11-08 10:50:32 +0100

    fix donor cell

--- src/discretization/2_donor_cell.cpp
+++ src/discretization/2_donor_cell.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "discretization/2_donor_cell.h"
 
 DonorCell::DonorCell(std::array<int, 2> nCells, std::array<double, 2> meshWidth, double alpha) :
@@ -10,27 +11,27 @@ DonorCell::DonorCell(std::array<int, 2> nCells, std::array<double, 2> meshWidth,
 //! compute the 1st derivative ∂ u^2 / ∂x
 double DonorCell::computeDu2Dx(int i, int j) const {
     double A = ((u(i,j) + u(i+1,j)) * (u(i,j) + u(i+1,j)) - (u(i-1,j) + u(i,j)) * (u(i-1,j) + u(i,j))) / (4 * Discretization::dx());
-    double B = (abs(u(i,j) + u(i+1,j)) * (u(i,j) - u(i+1,j)) - abs(u(i-1,j) + u(i,j)) * (u(i-1,j) - u(i,j))) / (4 * Discretization::dx());
-    return A + DonorCell::alpha_ * B;
+    double B = (fabs(u(i,j) + u(i+1,j)) * (u(i,j) - u(i+1,j)) - fabs(u(i-1,j) + u(i,j)) * (u(i-1,j) - u(i,j))) / (4 * Discretization::dx());
+    return A + alpha_ * B;
 }
 
 //! compute the 1st derivative ∂ v^2 / ∂y
 double DonorCell::computeDv2Dy(int i, int j) const {
     double A = ((v(i,j+1) + v(i,j)) * (v(i,j+1) + v(i,j)) - (v(i,j) + v(i,j-1)) * (v(i,j) + v(i,j-1))) / (4 * Discretization::dy());
-    double B = ((v(i,j) - v(i,j+1)) * abs(v(i,j+1) + v(i,j)) - (v(i,j-1) - v(i,j)) * abs(v(i,j) + v(i,j-1))) / (4 * Discretization::dy());
-    return A+ DonorCell::alpha_;
+    double B = ((v(i,j) - v(i,j+1)) * fabs(v(i,j+1) + v(i,j)) - (v(i,j-1) - v(i,j)) * fabs(v(i,j) + v(i,j-1))) / (4 * Discretization::dy());
+    return A + alpha_ * B;
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂x
 double DonorCell::computeDuvDx(int i, int j) const {
     double A = ((v(i+1,j) + v(i,j)) * (u(i,j+1) + u(i,j)) - (v(i,j) + v(i-1,j)) * (u(i-1,j+1) + u(i-1,j))) / (4 * Discretization::dx());
-    double B = ((v(i,j) - v(i+1,j)) * abs(u(i,j+1) + u(i,j)) - (v(i-1,j) - v(i,j)) * abs(u(i-1,j+1) + u(i-1,j))) / (4 * Discretization::dx());
-    return A+ DonorCell::alpha_;
+    double B = ((v(i,j) - v(i+1,j)) * fabs(u(i,j+1) + u(i,j)) - (v(i-1,j) - v(i,j)) * fabs(u(i-1,j+1) + u(i-1,j))) / (4 * Discretization::dx());
+    return A + alpha_ * B;
 }
 
 //! compute the 1st derivative ∂ (uv) / ∂y
 double DonorCell::computeDuvDy(int i, int j) const {
     double A = ((u(i,j+1) + u(i,j)) * (v(i+1,j) + v(i,j)) - (u(i,j) + u(i,j-1)) * (v(i+1,j-1) + v(i,j-1))) / (4 * Discretization::dy());
-    double B = ((u(i,j) - u(i,j+1)) * abs(v(i+1,j) + v(i,j)) - (u(i,j-1) - u(i,j-1)) * abs(v(i+1,j-1) + v(i,j-1))) / (4 * Discretization::dy());
-    return A + DonorCell::alpha_ * B;
+    double B = ((u(i,j) - u(i,j+1)) * fabs(v(i+1,j) + v(i,j)) - (u(i,j-1) - u(i,j-1)) * fabs(v(i+1,j-1) + v(i,j-1))) / (4 * Discretization::dy());
+    return A + alpha_ * B;
 }
