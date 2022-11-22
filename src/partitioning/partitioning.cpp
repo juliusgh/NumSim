9fd1651024137a332981638e2e0f364d4b23caf8
Julius Herb <julius.herb@gmail.com>
2022-11-22 13:38:00 +0100

    add partitioning documentation

--- src/partitioning/partitioning.cpp
+++ src/partitioning/partitioning.cpp
@@ -1,1 +1,6 @@
 #include "partitioning/partitioning.h"
+ /**
+  * should be able to tell the own rank number and the rank numbers of neighbouring processes,
+  * know whether the own subdomain touches one of the boundaries left, right, top or bottom of the global domain
+  * and it should know the number of cells in the local staggered grid of the own subdomain.
+  */
