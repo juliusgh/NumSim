88f17bffedd2dc6ea763f495f922382e6f7f3ede
nhornischer <st161340@stud.uni-stuttgart.de>
2023-01-17 21:45:57 +0100

    Add implemented discretizations

--- src/discretization/2_central_differences.h
+++ src/discretization/2_central_differences.h
@@ -11,31 +11,31 @@ class CentralDifferences : public Discretization
 public:
   CentralDifferences(std::array<int,2> nCells, std::array<double,2> meshWidth);
 
-    /**
-   * compute the 1st derivative ∂ u^2 / ∂x
-   * @param i: discretized position in x direcetion
-   * @param j: discretiszed position in y direction
-   * @return central differences derivative approximation of the derivative stated above
-   */
+  /**
+  * compute the 1st derivative ∂ u^2 / ∂x
+  * @param i: discretized position in x direcetion
+  * @param j: discretiszed position in y direction
+  * @return central differences derivative approximation of the derivative stated above
+  */
   virtual double computeDu2Dx(int i, int j) const;
 
-    /**
-   * compute the 1st derivative ∂ v^2 / ∂y
-   * @param i: discretized position in x direcetion
-   * @param j: discretiszed position in y direction
-   * @return central differences derivative approximation of the derivative stated above
-   */
+  /**
+  * compute the 1st derivative ∂ v^2 / ∂y
+  * @param i: discretized position in x direcetion
+  * @param j: discretiszed position in y direction
+  * @return central differences derivative approximation of the derivative stated above
+  */
   virtual double computeDv2Dy(int i, int j) const;
 
-    /**
-   * compute the 1st derivative ∂ (uv) / ∂x
-   * @param i: discretized position in x direcetion
-   * @param j: discretiszed position in y direction
-   * @return central differences derivative approximation of the derivative stated above
-   */
+  /**
+  * compute the 1st derivative ∂ (uv) / ∂x
+  * @param i: discretized position in x direcetion
+  * @param j: discretiszed position in y direction
+  * @return central differences derivative approximation of the derivative stated above
+  */
   virtual double computeDuvDx(int i, int j) const;
 
-    /**
+  /**
   * compute the 1st derivative ∂ (uv) / ∂y
   * @param i: discretized position in x direcetion
   * @param j: discretiszed position in y direction
@@ -43,20 +43,20 @@ public:
   */
   virtual double computeDuvDy(int i, int j) const;
 
-    /**
+  /**
    * compute the 1st derivative ∂ (ut) / ∂x
    * @param i: discretized position in x direcetion
    * @param j: discretiszed position in y direction
    * @return central differences derivative approximation of the derivative stated above
    */
-    virtual double computeDutDx(int i, int j) const;
+  virtual double computeDutDx(int i, int j) const;
 
-    /**
+  /**
   * compute the 1st derivative ∂ (vt) / ∂y
   * @param i: discretized position in x direcetion
   * @param j: discretiszed position in y direction
   * @return central differences derivative approximation of the derivative stated above
   */
-    virtual double computeDvtDy(int i, int j) const;
+  virtual double computeDvtDy(int i, int j) const;
   
 };
--- src/discretization/2_donor_cell.h
+++ src/discretization/2_donor_cell.h
@@ -11,19 +11,19 @@ class DonorCell : public Discretization
 public:
 
   /**
-   * use the constructor of the base class
-   * @param nCells: number of inner cells
-   * @param meshWidth: width of a cell in both directions
-   * @param alpha: donor cell weight parameter
-   */
-  DonorCell(std::array<int,2> nCells, std::array<double,2> meshWidth, double alpha);
+  * use the constructor of the base class
+  * @param nCells: number of inner cells
+  * @param meshWidth: width of a cell in both directions
+  * @param alpha: donor cell weight parameter
+  */
+  DonorCell(std::array<int,2> nCells, std::array<double,2> meshWidth, double alpha, double gamma);
 
   /**
-   * compute the 1st derivative ∂ u^2 / ∂x
-   * @param i: discretized position in x direcetion
-   * @param j: discretiszed position in y direction
-   * @return donor cell derivative approximation of the derivative stated above
-   */
+  * compute the 1st derivative ∂ u^2 / ∂x
+  * @param i: discretized position in x direcetion
+  * @param j: discretiszed position in y direction
+  * @return donor cell derivative approximation of the derivative stated above
+  */
   virtual double computeDu2Dx(int i, int j) const;
 
   /**
@@ -50,22 +50,23 @@ public:
   */
   virtual double computeDuvDy(int i, int j) const;
 
-    /**
-   * compute the 1st derivative ∂ (ut) / ∂x
-   * @param i: discretized position in x direcetion
-   * @param j: discretiszed position in y direction
-   * @return central differences derivative approximation of the derivative stated above
-   */
-    virtual double computeDutDx(int i, int j) const;
+  /**
+  * compute the 1st derivative ∂ (ut) / ∂x
+  * @param i: discretized position in x direcetion
+  * @param j: discretiszed position in y direction
+  * @return central differences derivative approximation of the derivative stated above
+  */
+  virtual double computeDutDx(int i, int j) const;
 
-    /**
+  /**
   * compute the 1st derivative ∂ (vt) / ∂y
   * @param i: discretized position in x direcetion
   * @param j: discretiszed position in y direction
   * @return central differences derivative approximation of the derivative stated above
   */
-    virtual double computeDvtDy(int i, int j) const;
+  virtual double computeDvtDy(int i, int j) const;
 
 private:
   double alpha_;
+  double gamma_;
 };
