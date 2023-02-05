812a9129d2296f2a2b5b42f214d9c0398c73f5a0
Torben Schiz <49746900+tjwsch@users.noreply.github.com>
2023-02-05 16:37:16 +0100

    Revert "add first cases for surface bc"
    This reverts commit 91e5ac03aa5b384baa3ebbad296d2d93dff28c45.

--- src/computation/0_computation.cpp
+++ src/computation/0_computation.cpp
@@ -109,7 +109,6 @@ void Computation::runSimulation() {
         discretization_->setObstacleValues();
 
         //TODO re-set all u, v and p values at the free surface accpording to the free surface conditions
-        discretization_->setSurfaceValues();
 
         trackParticles();
 
